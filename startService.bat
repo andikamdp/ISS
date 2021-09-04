99efad64956bada03b45233a8f5a2014bbe26500
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-04 23:49:25 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -1,1 +1,10 @@
-START java -jar Spring_Boot_Training-0.0.1-SNAPSHOT.jar
+REM START java -jar Spring_Boot_Training-0.0.1-SNAPSHOT.jar
+
+stage('build') {
+      cmd_exec('echo "Buils starting..."')
+      cmd_exec('echo The current directory is %CD%')
+}
+
+def cmd_exec(command) {
+    return bat(returnStdout: true, script: "${command}").trim()
+}
