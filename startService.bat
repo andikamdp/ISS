a16ffc75b2dfb95e1e5e221c498f95bcfbdfc51b
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-05 01:36:34 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -1,2 +1,4 @@
-start cmd /k echo Hello, World!
+echo Current Dir "%CD%"
+xcopy /s /Y c:\mmyinbox\test.doc E:\GIT\Test
+xcopy /s /Y c:\mmyinbox\test.doc E:\GIT\Test
 javaw -jar -Xms1024m -Xmx1024m Spring_Boot_Training-0.0.1-SNAPSHOT.jar
