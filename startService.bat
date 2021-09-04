a63911211ddc4cc6ac07110bd9a5976a1cbaa812
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-05 01:41:02 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -2,6 +2,6 @@ echo Current Dir "%CD%"
 xcopy /s /Y C:\Users\Andika Mulyawan\.jenkins\workspace\test E:\GIT\Test
 xcopy /s /Y C:\Users\Andika Mulyawan\.jenkins\workspace\test\target E:\GIT\Test
 
-cd e:
-cd 'E:\GIT\Test'
+cd /d E:
+cd "E:\GIT\Test"
 java -jar -Xms1024m -Xmx1024m Spring_Boot_Training-0.0.1-SNAPSHOT.jar
