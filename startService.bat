c669ba64d7ad04b2b31297095ee8b68b9e3449e6
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-14 21:50:38 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -1,6 +1,6 @@
 echo Current Dir "%CD%"
-xcopy /s /Y C:\Users\Andika Mulyawan\.jenkins\workspace\test E:\GIT\PUBLISH\Test
-xcopy /s /Y C:\Users\Andika Mulyawan\.jenkins\workspace\test\target E:\GIT\PUBLISH\Test
+xcopy /s /Y "C:\Users\Andika Mulyawan\.jenkins\workspace\test" E:\GIT\PUBLISH\Test
+xcopy /s /Y "C:\Users\Andika Mulyawan\.jenkins\workspace\test\target" E:\GIT\PUBLISH\Test
 
 cd /d E:
 cd "E:\GIT\PUBLISH\Test"
